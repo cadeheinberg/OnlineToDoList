d15019fe7d800da090e9f557a692856cc697fe42
cadeheinberg <cadeheinberg@outlook.com>
2024-08-20 04:28:57 +0000

    git ignore changes

---
+++ mern-todo/backend/db.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+const promisePool = pool.promise();
+
+module.exports = promisePool;
